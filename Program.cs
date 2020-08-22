25fca7ebb04d85af2d331fca2a1f7f30b815ead0
Aakrossh <Aakrossh7@gmail.com>
2020-08-22 16:19:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1_Beginners_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
